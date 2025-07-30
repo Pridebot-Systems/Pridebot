459966d78b02d5528c61f5f46bb6c2a174690ab7
Sdriver1 <sjdriver08@gmail.com>
2025-07-30 14:42:40 -0400

    Changing sharding amount per cluster

--- src/clustermanager.js
+++ src/clustermanager.js
@@ -3,7 +3,7 @@ const { ClusterManager } = require("discord-hybrid-sharding");
 
 const manager = new ClusterManager(`${__dirname}/index.js`, {
   totalShards: "auto",
-  shardsPerClusters: 3,
+  shardsPerClusters: 4,
   totalClusters: "auto",
   mode: "process",
   token: process.env.token,
