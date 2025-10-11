9ab5398c353fc6e537cee71047369a85d85aebe6
Sdriver1 <sjdriver08@gmail.com>
2025-10-11 14:15:17 -0400

    Cleaning up DB schemas

--- mongo/models/giveawaySchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
-
-const GiveawaySchema = new Schema({
-  entrants: { type: [String], default: [] },
-  endTime: { type: Date, required: true },
-});
-
-module.exports = { Giveaway: model("Giveaway", GiveawaySchema) };
