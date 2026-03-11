7b36451b3512f857dd057f9a21bf24ba9f6e3ed0
Sdriver1 <sjdriver08@gmail.com>
2026-03-11 05:54:52 -0400

    New profile badge for anyone who hits dar easter eggs

--- src/config/logging/darlog.js
+++ src/config/logging/darlog.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require("discord.js");
-const {sendLog} = require("./sendlogs");
+const { sendLog } = require("./sendlogs");
+const id = require("../../../mongo/models/idSchema");
 
 const darlogging = async (client, meterType, userName, meter, userId) => {
   const channelId = "1286437229920780371";
@@ -10,12 +11,24 @@ const darlogging = async (client, meterType, userName, meter, userId) => {
   if (meter > 500 || meter < -500) {
     specialMessage = `<@691506668781174824> WE HAVE A WIN!`;
     embedColor = 0xffd700;
+    const idLists = await id.findOne();
+    if (idLists) {
+      if (!idLists.easteregg.includes(userId)) {
+        idLists.easteregg.push(userId);
+        await idLists.save();
+      }
+    } else {
+      const newIdLists = new id({
+        easteregg: [userId],
+      });
+      await newIdLists.save();
+    }
   }
 
   const embed = new EmbedBuilder()
     .setTitle(`${meterType} Meter Result`)
     .setDescription(
-      `**User:** <@${userId}> (${userName})\n**Meter:** ${meter}%`
+      `**User:** <@${userId}> (${userName})\n**Meter:** ${meter}%`,
     )
     .setColor(embedColor)
     .setTimestamp();
